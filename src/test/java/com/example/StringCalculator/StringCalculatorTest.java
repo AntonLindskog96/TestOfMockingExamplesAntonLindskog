095d19f502c3cf03803ab15f71759dde23544998
Anton <lindskog96@live.se>
2023-01-30 09:57:51 +0100

    added test to check if adding two numbers equals the sum

--- src/test/java/com/example/StringCalculator/StringCalculatorTest.java
+++ src/test/java/com/example/StringCalculator/StringCalculatorTest.java
@@ -1,4 +1,27 @@
+package com.example.StringCalculator;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StringCalculatorTest {
-  
+
+    @Test
+    public void EmptyStringReturnsZero() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("");
+        assertEquals(0, result);
+    }
+    @Test
+    public void AddTwoNumbersToReturnTheSumOfTheNumbers (){
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1,2");
+        assertEquals(3,result) ;
+    }
+
+
+
+
+
+
 }
