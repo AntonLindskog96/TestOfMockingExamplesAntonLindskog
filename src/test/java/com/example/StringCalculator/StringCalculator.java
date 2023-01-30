c5a4b2c4f5e020991109941d9a6c2915b762cb22
Anton <lindskog96@live.se>
2023-01-30 09:49:07 +0100

    Added test to check if empty string returns zero

--- src/test/java/com/example/StringCalculator/StringCalculator.java
+++ src/test/java/com/example/StringCalculator/StringCalculator.java
@@ -1,2 +1,17 @@
-package com.example.StringCalculator;public class StringCalculator {
+package com.example.StringCalculator;
+
+public class StringCalculator {
+    public int add(String numbers) {
+        if (numbers == null || numbers.isEmpty()) {
+            return 0;
+        }
+        String[] parts = numbers.split(",");
+        int sum = 0;
+        for (String part : parts) {
+            sum += Integer.parseInt(part);
+        }
+        return sum;
+    }
+
+
 }
